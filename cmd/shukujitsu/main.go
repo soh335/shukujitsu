acf41472dfc1693e67d52980a9481b488cd8eceb
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2021-02-26 23:55:27 +0900

    Add goreleaser configurations.

--- cmd/shukujitsu/main.go
+++ cmd/shukujitsu/main.go
@@ -2,24 +2,32 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 
 	"github.com/soh335/shukujitsu"
 )
 
 var (
-	quiet      bool
-	date       string
-	dateFormat = "2006-01-02"
+	quiet       bool
+	date        string
+	dateFormat  = "2006-01-02"
+	showVersion bool
+	version     = "current"
 )
 
 func init() {
 	flag.BoolVar(&quiet, "quiet", false, "quiet")
 	flag.StringVar(&date, "date", "", "date (format "+dateFormat+")")
+	flag.BoolVar(&showVersion, "version", false, "show version")
 }
 
 func main() {
 	flag.Parse()
+	if showVersion {
+		fmt.Println("shukujitsu", version)
+		return
+	}
 	code := shukujitsu.RunCLI(&shukujitsu.CLIOption{
 		Quiet:      quiet,
 		Date:       date,
